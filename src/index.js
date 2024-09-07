1871781d2da9a05da71d0cb6268b18b639476646
Yuebo Wang <yuebooscar@gmail.com>
2024-09-07 15:54:57 -0700

    remove fontawesome import from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import App from './App';
 import './index.css';
 
